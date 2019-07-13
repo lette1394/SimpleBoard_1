7c9ae04ea864b8726611d356a6291eee707dc136
Jae Eun Na <lette1394@gmail.com>
2019-07-13 16:53:54 +0900

    Add Account controller

---
+++ src/main/java/com/lette1394/board/domain/Account/controller/AccountController.java
@@ -0,0 +1,22 @@
+package com.lette1394.board.domain.Account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+import com.lette1394.board.common.service.FindService;
+import com.lette1394.board.domain.User.model.User;
+import com.lette1394.board.domain.User.repository.UserRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+    private final FindService<User> userService;
+
+    @GetMapping("/")
+    public User getUser() {
+        final User one = userService.getOne(0);
+        System.out.println(one);
+        return null;
+    }
+}
